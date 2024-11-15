a3aabee1185b6507f81e0aebef35185e3e9979b9
JinYunhwa <dbsghk320@gmail.com>
2024-11-15 10:17:00 +0900

    -fix: 북마크 css수정

--- src/components/Chatbot/LeftSection/PaperItem.vue
+++ src/components/Chatbot/LeftSection/PaperItem.vue
@@ -5,14 +5,14 @@ import BookMarkIcon from '@/assets/BookMarkIcon.png'
 
 <template>
   <div class="paper-item d-flex align-items-center my-2">
-    <div>
+    <div class="p-3">
       <img :src="DragHandleIcon" />
     </div>
-    <div class="text-start">
+    <div class="text-start flex-grow-1 d-flex flex-column">
       <p>논문명</p>
       <p>텍스트-이미지 생성</p>
     </div>
-    <div>
+    <div class="ms-auto p-4">
       <img :src="BookMarkIcon" />
     </div>
   </div>
@@ -25,8 +25,11 @@ import BookMarkIcon from '@/assets/BookMarkIcon.png'
   border-radius: 10px;
   margin: 5px;
   padding: 0px;
+  height: 75px;
 }
 .paper-item p {
   line-height: 0.7;
+  margin-top: 5px;
+  margin-bottom: 5px;
 }
 </style>
