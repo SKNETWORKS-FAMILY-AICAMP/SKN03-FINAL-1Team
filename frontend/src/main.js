83f74ba691f9cc43897ae93f8302276427218d25
JinYunhwa <dbsghk320@gmail.com>
2024-11-21 09:59:14 +0900

    -add: main.js에 axios 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import pinia from './stores'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
 
 const app = createApp(App)
 
+app.use(axios)
 app.use(router)
 app.use(pinia)
 
