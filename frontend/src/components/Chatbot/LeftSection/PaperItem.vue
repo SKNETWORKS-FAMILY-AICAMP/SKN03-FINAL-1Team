a02d803d0860dc2ee4ca7b822677e84ecffd9e37
JinYunhwa <dbsghk320@gmail.com>
2024-11-25 14:12:09 +0900

    -test: 북마크테스트

--- frontend/src/components/Chatbot/LeftSection/PaperItem.vue
+++ frontend/src/components/Chatbot/LeftSection/PaperItem.vue
@@ -1,19 +1,47 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import axios from '@/axiosConfig' // 설정한 axios 인스턴스를 가져옵니다.
 import DragHandleIcon from '@/assets/DragHandleIcon.png'
 import BookMarkIcon from '@/assets/BookMarkIcon.png'
+
+const userId = 31525125
+const bookmarks = ref([])
+
+// 사용자 북마크 데이터 가져오기
+const fetchBookmarks = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.documento.click/papers/bookmarks?userId=${userId}`,
+    )
+    bookmarks.value = response.data.paperList
+    console.log('Bookmarks:', bookmarks.value)
+  } catch (error) {
+    console.error('Failed to fetch bookmarks:', error)
+  }
+}
+
+onMounted(() => {
+  fetchBookmarks()
+})
 </script>
 
 <template>
-  <div class="paper-item d-flex align-items-center my-2">
-    <div class="p-3">
-      <img :src="DragHandleIcon" />
-    </div>
-    <div class="text-start flex-grow-1 d-flex flex-column">
-      <p>논문명</p>
-      <p>텍스트-이미지 생성</p>
-    </div>
-    <div class="ms-auto p-4">
-      <img :src="BookMarkIcon" />
+  <div>
+    <div
+      v-for="(bookmark, index) in bookmarks"
+      :key="index"
+      class="paper-item d-flex align-items-center my-2"
+    >
+      <div class="p-3">
+        <img :src="DragHandleIcon" />
+      </div>
+      <div class="text-start flex-grow-1 d-flex flex-column">
+        <p>{{ bookmark.title }}</p>
+        <p>{{ bookmark.userKeyword }}</p>
+      </div>
+      <div class="ms-auto p-4">
+        <img :src="BookMarkIcon" />
+      </div>
     </div>
   </div>
 </template>
