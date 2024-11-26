4421273f155680a3ca64168a153ef695fc0796d1
JinYunhwa <dbsghk320@gmail.com>
2024-11-26 14:12:45 +0900

    Reapply "add: gitignore에 frontend\src\axiosConfig.js 추가"
    This reverts commit 2028b6543b9c7b2139b133a1b358c86286389a52.

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // Axios 인스턴스 생성
 const instance = axios.create({
-  // baseURL: 'http://localhost:8000', // API 서버 주소
-  baseURL: 'https://api.documento.click', // API 서버 주소
+  baseURL: 'http://localhost:8000', // API 서버 주소
+  // baseURL: 'https://api.documento.click', // API 서버 주소
   withCredentials: true, // 인증 정보를 포함하는 요청 허용
   headers: {
     'Content-Type': 'application/json',
