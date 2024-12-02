a4d01952ff577a6a766b3a861d958d534c413efe
JinYunhwa <dbsghk320@gmail.com>
2024-12-02 10:47:03 +0900

    test: 선행 논문 api 테스트

--- frontend/src/components/common/PriorPaperList.vue
+++ frontend/src/components/common/PriorPaperList.vue
@@ -1,14 +1,22 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import axios from '@/axiosConfig' // 설정한 axios 인스턴스를 가져옵니다.
 
 const papers = ref([])
+const route = useRoute()
 
 const fetchPriorPapers = async () => {
+  const paperDoi = route.query.paperDoi
+  if (!paperDoi) {
+    console.error('paperDoi가 쿼리에 존재하지 않습니다.')
+    return
+  }
+
   try {
     const response = await axios.get('/papers/priorpapers/', {
       params: {
-        paperDoi: '10.18653/v1/2020.acl-demos.10',
+        paperDoi: paperDoi,
       },
     })
     if (response.data.resultCode === 201 && response.data.result.paperList) {
@@ -18,12 +26,15 @@ const fetchPriorPapers = async () => {
     console.error('선행 논문을 가져오는 중 오류 발생:', error)
   }
 }
+
+onMounted(() => {
+  fetchPriorPapers()
+})
 </script>
 
 <template>
   <div class="container mt-4">
     <h2>선행 논문 목록</h2>
-    <button class="btn btn-primary mb-3" @click="fetchPriorPapers">선행 논문 불러오기</button>
     <ul class="list-group">
       <li
         v-for="paper in papers"
