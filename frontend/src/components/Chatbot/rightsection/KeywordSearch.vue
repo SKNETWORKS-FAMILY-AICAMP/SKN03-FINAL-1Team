eb9d8b0f054a29b142529f40a9b9cb469be2833b
JinYunhwa <dbsghk320@gmail.com>
2024-12-05 14:51:50 +0900

    test: mockResponce 추가

--- frontend/src/components/Chatbot/rightsection/KeywordSearch.vue
+++ frontend/src/components/Chatbot/rightsection/KeywordSearch.vue
@@ -20,6 +20,130 @@ const steps = [
 
 const accessToken = 'temp' // 실제 토큰을 할당
 
+const mockResponse = {
+  resultCode: 201,
+  message: 'Keyword completed successfully',
+  result: {
+    generatedPrompt:
+      '"Efficient fine-tuning strategies for large-scale pre-trained language models"의 키워드 검색 결과입니다.\n\n',
+    generatedKeywordList: [
+      {
+        generatedKeyword:
+          'Performance enhancement of pre-trained models [사전 학습된 모델의 성능 향상]',
+        paperList: [
+          {
+            paperDoi: '10.18653/v1/2023.acl-long.437',
+            title:
+              'On the Evaluation of Neural Selective Prediction Methods for Natural Language Processing',
+            engAbstract:
+              'We provide a survey and empirical comparison of the state-of-the-art in neural selective classification for NLP tasks. We also provide a methodological blueprint, including a novel metric called refinement that provides a calibrated evaluation of confidence functions for selective prediction. Finally, we supply documented, open-source code to support the future development of selective prediction techniques.',
+            citation: 0,
+          },
+          {
+            paperDoi: '10.18653/v1/2020.acl-demos.15',
+            title:
+              'jiant: A Software Toolkit for Research on General-Purpose Text Understanding Models',
+            engAbstract:
+              'We introduce jiant, an open source toolkit for conducting multitask and transfer learning experiments on English NLU tasks. jiant enables modular and configuration driven experimentation with state-of-the-art models and a broad set of tasks for probing, transfer learning, and multitask training experiments. jiant implements over 50 NLU tasks, including all GLUE and SuperGLUE benchmark tasks. We demonstrate that jiant reproduces published performance on a variety of tasks and models, e.g., RoBERTa and BERT.',
+            citation: 92,
+          },
+          {
+            paperDoi: '10.18653/v1/2024.acl-long.726',
+            title: 'Advancing Parameter Efficiency in Fine-tuning via Representation Editing',
+            engAbstract:
+              'Parameter Efficient Fine-Tuning (PEFT) has gained significant attention for its ability to achieve competitive results while updating only a small subset of trainable parameters. Despite the promising performance of current PEFT methods, they present challenges in hyperparameter selection, such as determining the rank of LoRA or Adapter, or specifying the length of soft prompts. In addressing these challenges, we propose a novel approach to fine-tuning neural models, termed Representation EDiting (RED), which scales and biases the representation produced at each layer. RED substantially reduces the number of trainable parameters by a factor of 25,700 compared to full parameter fine-tuning, and by a factor of 32 compared to LoRA. Remarkably, RED achieves comparable or superior results to full parameter fine-tuning and other PEFT methods. Extensive experiments were conducted across models of varying architectures and scales, including RoBERTa, GPT-2, T5, and Llama-2, and the results demonstrate the efficiency and efficacy of RED, positioning it as a promising PEFT approach for large neural models.',
+            citation: 14,
+          },
+        ],
+      },
+      {
+        generatedKeyword: 'Large-scale language model optimization [대규모 언어 모델 최적화]',
+        paperList: [
+          {
+            paperDoi: '10.18653/v1/2024.acl-long.845',
+            title: 'Aya Model: An Instruction Finetuned Open-Access Multilingual Language Model',
+            engAbstract:
+              'Recent breakthroughs in large language models (LLMs) have centered around a handful of data-rich languages. What does it take to broaden access to breakthroughs beyond first-class citizen languages? Our work introduces Aya, a massively multilingual generative language model that follows instructions in 101 languages of which over 50% are considered as lower-resourced. Aya outperforms mT0 and BLOOMZ on the majority of tasks while covering double the number of languages. We introduce extensive new evaluation suites that broaden the state-of-art for multilingual eval across 99 languages —— including discriminative and generative tasks, human evaluation, and simulated win rates that cover both held-out tasks and in-distribution performance. Furthermore, we conduct detailed investigations on the optimal finetuning mixture composition, data pruning, as well as the toxicity, bias, and safety of our models.',
+            citation: 116,
+          },
+          {
+            paperDoi: '10.18653/v1/2023.acl-tutorials.3',
+            title:
+              'Everything you need to know about Multilingual LLMs: Towards fair, performant and reliable models for languages of the world',
+            engAbstract:
+              'This tutorial will describe various aspects of scaling up language technologies to many of the world’s languages by describing the latest research in Massively Multilingual Language Models (MMLMs). We will cover topics such as data collection, training and fine-tuning of models, Responsible AI issues such as fairness, bias and toxicity, linguistic diversity and evaluation in the context of MMLMs, specifically focusing on issues in non-English and low-resource languages. Further, we will also talk about some of the real-world challenges in deploying these models in language communities in the field. With the performance of MMLMs improving in the zero-shot setting for many languages, it is now becoming feasible to use them for building language technologies in many languages of the world, and this tutorial will provide the computational linguistics community with unique insights from the latest research in multilingual models.',
+            citation: 7,
+          },
+          {
+            paperDoi: '10.18653/v1/2020.acl-main.417',
+            title: 'ParaCrawl: Web-Scale Acquisition of Parallel Corpora',
+            engAbstract:
+              'We report on methods to create the largest publicly available parallel corpora by crawling the web, using open source software. We empirically compare alternative methods and publish benchmark data sets for sentence alignment and sentence pair filtering. We also describe the parallel corpora released and evaluate their quality and their usefulness to create machine translation systems.',
+            citation: 227,
+          },
+        ],
+      },
+      {
+        generatedKeyword:
+          'Deep Learning model fine-tuning strategies [딥 러닝 모델 세부 조정 전략]',
+        paperList: [
+          {
+            paperDoi: '10.18653/v1/2020.acl-demos.15',
+            title:
+              'jiant: A Software Toolkit for Research on General-Purpose Text Understanding Models',
+            engAbstract:
+              'We introduce jiant, an open source toolkit for conducting multitask and transfer learning experiments on English NLU tasks. jiant enables modular and configuration driven experimentation with state-of-the-art models and a broad set of tasks for probing, transfer learning, and multitask training experiments. jiant implements over 50 NLU tasks, including all GLUE and SuperGLUE benchmark tasks. We demonstrate that jiant reproduces published performance on a variety of tasks and models, e.g., RoBERTa and BERT.',
+            citation: 92,
+          },
+          {
+            paperDoi: '10.18653/v1/2023.acl-long.437',
+            title:
+              'On the Evaluation of Neural Selective Prediction Methods for Natural Language Processing',
+            engAbstract:
+              'We provide a survey and empirical comparison of the state-of-the-art in neural selective classification for NLP tasks. We also provide a methodological blueprint, including a novel metric called refinement that provides a calibrated evaluation of confidence functions for selective prediction. Finally, we supply documented, open-source code to support the future development of selective prediction techniques.',
+            citation: 0,
+          },
+          {
+            paperDoi: '10.18653/v1/2024.acl-long.726',
+            title: 'Advancing Parameter Efficiency in Fine-tuning via Representation Editing',
+            engAbstract:
+              'Parameter Efficient Fine-Tuning (PEFT) has gained significant attention for its ability to achieve competitive results while updating only a small subset of trainable parameters. Despite the promising performance of current PEFT methods, they present challenges in hyperparameter selection, such as determining the rank of LoRA or Adapter, or specifying the length of soft prompts. In addressing these challenges, we propose a novel approach to fine-tuning neural models, termed Representation EDiting (RED), which scales and biases the representation produced at each layer. RED substantially reduces the number of trainable parameters by a factor of 25,700 compared to full parameter fine-tuning, and by a factor of 32 compared to LoRA. Remarkably, RED achieves comparable or superior results to full parameter fine-tuning and other PEFT methods. Extensive experiments were conducted across models of varying architectures and scales, including RoBERTa, GPT-2, T5, and Llama-2, and the results demonstrate the efficiency and efficacy of RED, positioning it as a promising PEFT approach for large neural models.',
+            citation: 14,
+          },
+        ],
+      },
+      {
+        generatedKeyword:
+          'Advanced adaptation of pre-trained models [사전 학습된 모델의 고급 적응]',
+        paperList: [
+          {
+            paperDoi: '10.18653/v1/2022.acl-long.264',
+            title: 'The Trade-offs of Domain Adaptation for Neural Language Models',
+            engAbstract:
+              'This work connects language model adaptation with concepts of machine learning theory. We consider a training setup with a large out-of-domain set and a small in-domain set. We derive how the benefit of training a model on either set depends on the size of the sets and the distance between their underlying distributions. We analyze how out-of-domain pre-training before in-domain fine-tuning achieves better generalization than either solution independently. Finally, we present how adaptation techniques based on data selection, such as importance sampling, intelligent data selection and influence functions, can be presented in a common framework which highlights their similarity and also their subtle differences.',
+            citation: 18,
+          },
+          {
+            paperDoi: '10.18653/v1/2020.acl-demos.15',
+            title:
+              'jiant: A Software Toolkit for Research on General-Purpose Text Understanding Models',
+            engAbstract:
+              'We introduce jiant, an open source toolkit for conducting multitask and transfer learning experiments on English NLU tasks. jiant enables modular and configuration driven experimentation with state-of-the-art models and a broad set of tasks for probing, transfer learning, and multitask training experiments. jiant implements over 50 NLU tasks, including all GLUE and SuperGLUE benchmark tasks. We demonstrate that jiant reproduces published performance on a variety of tasks and models, e.g., RoBERTa and BERT.',
+            citation: 92,
+          },
+          {
+            paperDoi: '10.18653/v1/2022.acl-demo.26',
+            title: 'Adaptor: Objective-Centric Adaptation Framework for Language Models',
+            engAbstract:
+              'This paper introduces Adaptor library, which transposes traditional model-centric approach composed of pre-training + fine-tuning steps to objective-centric approach, composing the training process by applications of selected objectives. We survey research directions that can benefit from enhanced objective-centric experimentation in multitask training, custom objectives development, dynamic training curricula, or domain adaptation. Adaptor aims to ease reproducibility of these research directions in practice. Finally, we demonstrate the practical applicability of Adaptor in selected unsupervised domain adaptation scenarios.',
+            citation: 10,
+          },
+        ],
+      },
+    ],
+  },
+}
+
 // 키워드 최적화 요청 (POST 요청)
 const optimizeKeywords = async () => {
   showIntroAndSteps.value = false
