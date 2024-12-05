8095c95dc58a5cef82537b0c6a8f57707a87db0c
JinYunhwa <dbsghk320@gmail.com>
2024-12-05 17:37:25 +0900

    fest: pdf테스트

--- frontend/src/views/PaperDetailView.vue
+++ frontend/src/views/PaperDetailView.vue
@@ -3,60 +3,60 @@ import { ref, watch, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import axios from '@/axiosConfig'
 
-const paper = ref(null)
-const route = useRoute()
-const showPdfViewer = ref(false)
-const pdfUrl = ref('')
-const paperS3Path = ref('')
-const pdfFile = ref(null) // 추가
-const priorPapers = ref(null) // 추가: priorPapers 데이터를 저장할 변수
-const accessToken = 'temp' // 실제 토큰을 할당
+// const paper = ref(null)
+// const route = useRoute()
+// const showPdfViewer = ref(false)
+// const pdfUrl = ref('')
+// const paperS3Path = ref('')
+// const pdfFile = ref(null) // 추가
+// const priorPapers = ref(null) // 추가: priorPapers 데이터를 저장할 변수
+// const accessToken = 'temp' // 실제 토큰을 할당
 
-const fetchPaperDetails = async (paperDoi) => {
-  try {
-    const response = await axios.get(`/papers/select/`, {
-      params: { paperDoi },
-      headers: {
-        Authorization: `Bearer ${accessToken}`, // Authorization 헤더에 토큰 포함
-      },
-    })
-    if (response.data && response.data.resultCode === 200) {
-      paperS3Path.value = response.data.result.paperS3Path
-    } else {
-      console.error('Failed to fetch paper details')
-    }
-  } catch (error) {
-    console.error('Error fetching paper details:', error)
-  }
-}
+// const fetchPaperDetails = async (paperDoi) => {
+//   try {
+//     const response = await axios.get(`/papers/select/`, {
+//       params: { paperDoi },
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`, // Authorization 헤더에 토큰 포함
+//       },
+//     })
+//     if (response.data && response.data.resultCode === 200) {
+//       paperS3Path.value = response.data.result.paperS3Path
+//     } else {
+//       console.error('Failed to fetch paper details')
+//     }
+//   } catch (error) {
+//     console.error('Error fetching paper details:', error)
+//   }
+// }
 
-const fetchPdf = async (url) => {
-  try {
-    const response = await fetch(url)
-    const blob = await response.blob()
-    pdfFile.value = URL.createObjectURL(blob)
-    showPdfViewer.value = true
-  } catch (error) {
-    console.error('Error fetching PDF:', error)
-  }
-}
+// const fetchPdf = async (url) => {
+//   try {
+//     const response = await fetch(url)
+//     const blob = await response.blob()
+//     pdfFile.value = URL.createObjectURL(blob)
+//     showPdfViewer.value = true
+//   } catch (error) {
+//     console.error('Error fetching PDF:', error)
+//   }
+// }
 
-onMounted(async () => {
-  const paperDoi = route.query.paperDoi
-  if (paperDoi) {
-    await fetchPaperDetails(paperDoi)
-    // await fetchPriorPapers(paperDoi)
-  } else {
-    console.warn('paperDoi 쿼리 파라미터가 없습니다.')
-  }
-})
+// onMounted(async () => {
+//   const paperDoi = route.query.paperDoi
+//   if (paperDoi) {
+//     await fetchPaperDetails(paperDoi)
+//     // await fetchPriorPapers(paperDoi)
+//   } else {
+//     console.warn('paperDoi 쿼리 파라미터가 없습니다.')
+//   }
+// })
 
-// paperS3Path가 변경될 때마다 fetchPdf를 호출
-watch(paperS3Path, async (newPath) => {
-  if (newPath) {
-    await fetchPdf(newPath)
-  }
-})
+// // paperS3Path가 변경될 때마다 fetchPdf를 호출
+// watch(paperS3Path, async (newPath) => {
+//   if (newPath) {
+//     await fetchPdf(newPath)
+//   }
+// })
 </script>
 
 <template>
